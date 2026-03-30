39b5d02f88ff42a28807775f5feaaa301e44b832
pslcorrea <pslcorrea@gmail.com>
2026-03-30 17:56:48 -0300

    feat: Adicionado React router dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export {};
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<div>Home</div>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
